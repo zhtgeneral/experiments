98944f58df7adf2bed6c985b3ee237cfc49cac1a
Zhong Tan <zhtgeneral@gmail.com>
2024-08-24 13:10:54 -0700

    growthbook now tracks many browser info

--- app/page.tsx
+++ app/page.tsx
@@ -12,17 +12,53 @@ export default function Home() {
       // below makes a new exp id each refresh
       const userId = Math.random().toString(36).substring(2, 15);
 
-      // below gets the browser name
-      const browser = Bowser.getParser(window.navigator.userAgent).getBrowserName().toLowerCase();
+      // below gets info
+      const browser        = Bowser.getParser(window.navigator.userAgent).getBrowserName().toLowerCase();
+      const browserVersion = Bowser.getParser(window.navigator.userAgent).getBrowserVersion();
+      const os             = Bowser.getParser(window.navigator.userAgent).getOSName().toLowerCase();
+      const engine         = Bowser.getParser(window.navigator.userAgent).getEngineName().toLowerCase();
+      const platformType   = Bowser.getParser(window.navigator.userAgent).getPlatformType().toLowerCase();
 
-      growthbook.setAttributes({
-        id: userId,
-        browser: browser,
-      });
-
-      await growthbook.init({
-        streaming: true,
-      });
+      // below gets location
+      var location: string;
+      async function setAttributesInit(location: string) {
+        growthbook.setAttributes({
+          id: userId,
+          browser: browser,
+          browserVersion: browserVersion,
+          os: os,
+          engine: engine,
+          deviceType: platformType,
+          location: location
+        });
+        console.log('attributes: ', growthbook.getAttributes());
+        await growthbook.init({
+          streaming: true,
+        }); 
+      }
+      function parseLocation(position: GeolocationPosition) {
+        location = position.coords.latitude + "," + position.coords.longitude;
+        console.log('location: ', location);
+        setAttributesInit(location)
+      }
+      function parseError(error: GeolocationPositionError) {
+        switch(error.code) {
+          case error.PERMISSION_DENIED:
+            location = "denied"
+            break;
+          case error.POSITION_UNAVAILABLE:
+            location = "unavailable"
+            break;
+          case error.TIMEOUT:
+            location = "timed out"
+            break;
+          default:
+            location = "error";
+            break;
+        }
+        setAttributesInit(location)
+      }
+      navigator.geolocation.getCurrentPosition(parseLocation, parseError);
     }
     init();
   }, []);
