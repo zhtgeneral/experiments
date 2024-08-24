94e247d83d7cd4bcb0632d1c3b4524068e21de32
Zhong Tan <zhtgeneral@gmail.com>
2024-08-24 12:32:53 -0700

    added tracking for browser type

--- app/page.tsx
+++ app/page.tsx
@@ -4,38 +4,35 @@ import TestComponent from "@/components/TestComponent";
 import growthbook from "@/lib/growthbook";
 import { GrowthBookProvider } from "@growthbook/growthbook-react";
 import { useEffect } from "react";
+import * as Bowser from "bowser"
 
 export default function Home() {
   useEffect(() => {
     async function init() {
-      let userId = localStorage.getItem('userId');
       // // below saves the exp id in cache
+      // let userId = localStorage.getItem('userId');
       // if (!userId) {
       //   userId = Math.random().toString(36).substring(2, 15);
       //   localStorage.setItem('userId', userId);
       // }
+      // const userId = Math.random().toString(36).substring(2, 15);
+
       // below makes a new exp id each refresh
-      userId = Math.random().toString(36).substring(2, 15);
+      const userId = Math.random().toString(36).substring(2, 15);
+
+      // below gets the browser name
+      const browser = Bowser.getParser(window.navigator.userAgent).getBrowserName().toLowerCase();
 
       growthbook.setAttributes({
         id: userId,
+        browser: browser,
       });
+      
       await growthbook.init({
         streaming: true,
       });
-      // console.log("init", res);
-      // console.log("exp", growthbook.getExperiments());
-      console.log("feats", growthbook.getFeatures());
-      // console.log("api key", growthbook.getApiInfo()); // working
     }
     init();
-    
-    // // below works as well
-    // async function wait() {
-    //   const output = await axios.get('https://cdn.growthbook.io/api/features/sdk-fZaqCZwgoL08szNx')
-    //   console.log("output", output);
-    // }
-    // wait()
   }, []);
   
   return (
