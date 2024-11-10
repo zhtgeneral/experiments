df6cb584816d059601404c5bbc6fd4a121d95832
Zhong <zhtgeneral@gmail.com>
2024-11-09 21:35:10 -0800

    update frontend margins

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -5,7 +5,7 @@
 const Nav = () => {
   return (
     <nav className="bg-gradient-to-b from-nav-start to-nav-end w-full py-8 items-center flex justify-center">
-      <div className="font-bold text-5xl text-black text-center">
+      <div className="font-bold text-4xl lg:text-5xl text-black text-center">
         Experiment Web Page
       </div>
     </nav>
